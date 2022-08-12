eedef4e63a5cbaa873bd3418715fe9d9864aa324
Nabeel Ahmad <nab34923@gmail.com>
2022-08-13 04:27:26 +0500

    initial commit

---
+++ assets/data/categoriesData.jsx
@@ -0,0 +1,24 @@
+const categoriesData = [
+    {
+        id: '1',
+        image: require('../images/pizza-icon.png'),
+        title: "Pizza",
+        selected: true,
+
+    },
+    {
+        id: '2',
+        image: require('../images/shrimp-icon.png'),
+        title: "SeaFood",
+        selected: false,
+
+    },
+    {
+        id: '3',
+        image: require('../images/soda-icon.png'),
+        title: "Soft drink",
+        selected: false,
+
+    },
+]
+export default categoriesData;
